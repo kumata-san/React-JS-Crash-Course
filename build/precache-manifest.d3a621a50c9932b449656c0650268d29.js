e0fef35535c351a073ea0b67a2091b2c8e5b472b
kumata-san <kumatasan2019@gmail.com>
2019-12-08 02:41:30 +0900

    first commit

---
+++ build/precache-manifest.d3a621a50c9932b449656c0650268d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a683f63d26fb893ae0b7a1dc7e928f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1b3e5f10276e6a93a3a",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "d3b40856c55b0bb80f81",
+    "url": "/static/js/2.7a08a8f5.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.7a08a8f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1b3e5f10276e6a93a3a",
+    "url": "/static/js/main.5b8871bc.chunk.js"
+  },
+  {
+    "revision": "9831498b5744f1809604",
+    "url": "/static/js/runtime-main.ff4b91a6.js"
+  }
+]);
